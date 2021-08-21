76034f97dbe938bdb5fba06dd4ab51631ed418c8
Mohammed Yasser <mohammedre4a@gmail.com>
2021-08-21 20:40:32 +0200

    Add: add initial home page ( UI may be improved + add menu).

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:news/BackgroundPattern.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,25 +11,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'News app'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -35,68 +27,139 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        centerTitle: true,
+        toolbarHeight: MediaQuery.of(context).size.height * 0.1,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(45),
+          ),
+        ),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            BackgroundPattern(),
+            Container(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text('Pick your category\nof interest',
+                        style: TextStyle(
+                            fontSize: 26,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 79, 90, 105)),
+                        textAlign: TextAlign.start),
+                  ),
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Category('Sports', 'assets/images/sports.png',
+                                false, Color.fromARGB(255, 201, 28, 34)),
+                            Category('Politics', 'assets/images/Politics.png',
+                                true, Color.fromARGB(255, 0, 62, 144)),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Category('Health', 'assets/images/health.png',
+                                false, Color.fromARGB(255, 237, 30, 121)),
+                            Category('Business', 'assets/images/business.png',
+                                true, Color.fromARGB(255, 207, 126, 72)),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Category(
+                                'Environment',
+                                'assets/images/environment.png',
+                                false,
+                                Color.fromARGB(255, 72, 130, 207)),
+                            Category('Science', 'assets/images/science.png',
+                                true, Color.fromARGB(255, 242, 211, 82)),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class Category extends StatelessWidget {
+  final String categoryName, imagePath;
+  final Color color;
+  final bool isRightCategory;
+  final double borderRadius = 25.0;
+
+  Category(this.categoryName, this.imagePath, this.isRightCategory, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      height: 170,
+      decoration: BoxDecoration(
+        //if the category is right one then it won't have left border radius and vice versa
+        color: this.color,
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(borderRadius),
+            topRight: Radius.circular(borderRadius),
+            bottomLeft: isRightCategory
+                ? Radius.circular(0)
+                : Radius.circular(borderRadius),
+            bottomRight: isRightCategory
+                ? Radius.circular(borderRadius)
+                : Radius.circular(0)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Image.asset(
+                this.imagePath,
+                fit: BoxFit.contain,
+              ),
+            ),
+            Expanded(
+              child: Text(
+                this.categoryName,
+                style: TextStyle(color: Colors.white, fontSize: 22),
+              ),
+              flex: 1,
+            )
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+        ),
+      ),
     );
   }
 }
