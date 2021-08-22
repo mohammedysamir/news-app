741c2374441f1acdfd4dc05225a7bfce62f603a7
Mohammed Yasser <mohammedre4a@gmail.com>
2021-08-22 02:15:02 +0200

    Add: added route name to navigate through app, extract main code into splash.dart

--- lib/Home.dart
+++ lib/Home.dart
@@ -2,8 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:news/BackgroundPattern.dart';
 
-void main() => runApp(MyApp());
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -21,6 +19,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
+  static String routeName = 'Home';
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
