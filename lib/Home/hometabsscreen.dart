f98f80c9999a3e5bc9cd4fa7ff760c829f394b98
hopeandlearn <anabilsalah@gmail.com>
2021-08-24 18:26:21 +0200

    bug fix news api

---
+++ lib/Home/hometabsscreen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:news/Home/newsfragment.dart';
+import 'package:news/Home/tabitem.dart';
+import 'package:news/model/sourceresponse.dart';
+// ignore: use_key_in_widget_constructors
+class HomeTabs  extends StatefulWidget {
+  final List<Source> sources;
+  HomeTabs(this.sources);
+  @override
+  _HomeTabsState createState() => _HomeTabsState();
+}
+class _HomeTabsState extends State<HomeTabs> { 
+  int selectedindex=0;
+  @override
+  Widget build(BuildContext context) {
+    // print(widget.sources);
+    return DefaultTabController(
+      length:widget.sources.length ,
+      child: Container(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          children: [
+            TabBar(
+              onTap: (index){
+
+                print(index);
+                setState(() {
+                   selectedindex=index;
+                });
+              },
+              indicatorColor: Colors.transparent,
+              isScrollable: true,
+              tabs:widget.sources.map((source) => TabItem(source,widget.sources.indexOf(source)==selectedindex)).toList(),
+            ),
+            Expanded(
+              child: TabBarView(
+                children: widget.sources.map((source) => NewsFragment(source)).toList(),
+              ) ),],
+        ), ),);
+  }}
