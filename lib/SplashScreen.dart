87adef92b62f2849db7112fc8eaae3310503acc4
Mohammed Yasser <mohammedre4a@gmail.com>
2021-09-04 19:18:57 +0200

    refactor: improve splashScreen of the app.

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -63,37 +63,15 @@ class _SplashState extends State<SplashScreen> {
         body: Stack(
           children: [
             Container(
-              child: Image.asset("assets/images/pattern.png", fit: BoxFit.fill),
+              child: Image.asset("assets/images/splash.png", fit: BoxFit.fill),
               width: MediaQuery
                   .of(context)
                   .size
                   .width,
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(8.0, 200.0, 8.0, 0),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset("assets/images/logo.png"),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(8.0, 50.0, 8.0, 0),
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Image.asset("assets/images/route.png"),
-                              Text(
-                                "supervised by Mohamed Nabil",
-                                style: TextStyle(
-                                  color: Color.fromARGB(255, 57, 165, 82),
-                                  fontSize: 18,
-                                ),
-                              )
-                            ]),
-                      )
-                    ]),
-              ),
+              height:MediaQuery
+                  .of(context)
+                  .size
+                  .height,
             ),
           ],
         ));
